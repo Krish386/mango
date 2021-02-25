e74a15f8331df8c3663778db1947694b5b923ff3
Krish386 <75026067+Krish386@users.noreply.github.com>
2021-02-25 21:14:36 +0300

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,17 @@
+class Stone extends BaseClass
+{
+    constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("images/stone.png");
+  }
+
+  display() {
+ 
+    super.display();
+  }
+}
+
+  
+
+
+
